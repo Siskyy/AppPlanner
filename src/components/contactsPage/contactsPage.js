ac2d6258f1b08a4cf2456c29360c0dfeddf4c8a9
Siskyy <marcbertelli7@gmail.com>
2022-04-06 15:43:19 +0100

    linked props and setup onSubmit function

--- src/components/contactsPage/contactsPage.js
+++ src/components/contactsPage/contactsPage.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useState, useEffect} from 'react';
 
-const ContactsPage = () => {
+
+const ContactsPage = (props) => {
+
+    const contacts = props.contacts;
+    const addContact = props.addContact;
+
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [duplicate, setDuplicate] = useState(false);
+
+    const hadndleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!duplicate) {
+            addContact(name, phone, email);
+
+            /* clear from after submission */
+            setName('');
+            setPhone('');
+            setEmail('');
+        }
+    };
+
 
     return (
         <div>
-            
+
         </div>
     );
 };
