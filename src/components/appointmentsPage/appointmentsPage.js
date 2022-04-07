1373fade533d376c01d7140e4f35da4851a3dff9
Siskyy <marcbertelli7@gmail.com>
2022-04-07 15:02:01 +0100

    Appointments page with submit handle fucntion

--- src/components/appointmentsPage/appointmentsPage.js
+++ src/components/appointmentsPage/appointmentsPage.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AppointmentsPage = () => {
+import { AppointmentForm } from '../appointmentForm/AppointmentForm';
+import { TileList } from '../tileList/TileList';
+
+const AppointmentsPage = ({appointments, addAppointment, contacts}) => {
+
+    const [title, setTitle] = useState('');
+    const [contact, setContact] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addAppointment(title, contact, date, time);
+        setTitle('');
+        setContact('');
+        setDate('');
+        setTime('');
+    }
 
     return (
         <div>
